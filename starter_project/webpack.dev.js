b433685f25b804e7aab8fe5dd5556619c2e59625
RamiDarras <160128830+RamiDarras@users.noreply.github.com>
2025-01-28 18:06:21 +0200

    edited

--- starter_project/webpack.dev.js
+++ starter_project/webpack.dev.js
@@ -1,38 +1,50 @@
-const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  devtool: 'source-map',
-  stats: 'minimal',
-  entry: './src/client/index.js',
-  output: {
-    libraryTarget: 'var',
-    library: 'Client'
-  },
-  devServer: {
-    // automatically open browser
-    open: true,
-    // let devServer run on port 8080
-    port: 8080,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "babel-loader"
-      },
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+ mode: "development",
+ devtool: "source-map",
+ stats: "minimal",
+ entry: "./src/client/index.js",
+ output: {
+  libraryTarget: "var",
+  library: "Client"
+ },
+ devServer: {
+  // automatically open browser
+  open: true,
+  // let devServer run on port 8080
+  port: 8080
+ },
+ module: {
+  rules: [
+   {
+    test: /\.js$/,
+    exclude: /node_modules/,
+    loader: "babel-loader"
+   },
+   {
+    test: /\.scss$/,
+    use: [
+     process.env.NODE_ENV !== "production"
+      ? "style-loader"
+      : MiniCssExtractPlugin.loader,
+     "css-loader",
+     {
+      loader: "sass-loader",
+      options: {
+       // Prefer Dart Sass
+       implementation: require("sass")
       }
+     }
     ]
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: './src/client/views/index.html',
-      filename: 'index.html'
-    })
+   }
   ]
+ },
+ plugins: [
+  new HtmlWebPackPlugin({
+   template: "./src/client/views/index.html",
+   filename: "index.html"
+  })
+ ]
 };
